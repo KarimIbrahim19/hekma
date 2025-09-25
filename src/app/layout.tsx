13dd258bbd0c278cf5f04eb065c5f209f991a7c1
Karim Ibrahim <KarimIbrahim19@gmail.com>
2025-09-25 07:43:51 +0000

    yes this will be amazing and tell me how to use it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,15 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'PharmaLink',
   description: 'Pharmacy Management App',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'PharmaLink',
+  },
+  formatDetection: {
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
@@ -18,6 +27,27 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="application-name" content="PharmaLink" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PharmaLink" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#ADD8E6" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#F5F5F5" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#ADD8E6" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className="font-body antialiased">
         {children}
