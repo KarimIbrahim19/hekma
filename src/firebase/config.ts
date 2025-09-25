3c85ad064edfdcc8749ccf471a9e33cbaf54b3f8
Karim Ibrahim <KarimIbrahim19@gmail.com>
2025-09-25 07:52:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5672098592-83658",
+  "appId": "1:534413746683:web:673ecac2a45ad582a12b5e",
+  "apiKey": "AIzaSyBQ3GDNhfL82SNsE_SoExHymlgOcvpWblc",
+  "authDomain": "studio-5672098592-83658.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "534413746683"
+};
